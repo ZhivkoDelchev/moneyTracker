1cd2338ce0f0e621bc770022ea26348e59e1c787
Jako <Jako8809@gmail.com>
2015-08-26 22:23:53 +0300

    Remove one more strange symbol.

--- webapp/src/main/java/com/jako/moneytracker/exception/MyException.java
+++ webapp/src/main/java/com/jako/moneytracker/exception/MyException.java
@@ -1,7 +1,7 @@
 package com.jako.moneytracker.exception;
 
 /**
- * Created by Jako on 8.4.2015 ã..
+ * Created by Jako on 8.4.2015.
  */
 public class MyException extends RuntimeException {
     private static final long serialVersionUID = 2184023945190062719L;
