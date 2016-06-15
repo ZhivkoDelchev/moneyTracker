107d611abc707ff9ec52553ad3e14c9b69fbbd3b
Jako <Jako8809@gmail.com>
2016-06-15 08:54:49 +0300

    Delete java ee projects.

--- core/src/main/java/com/jako/moneytracker/exception/InvalidPaymentInputException.java
+++
@@ -1,9 +0,0 @@
-package com.jako.moneytracker.exception;
-
-public class InvalidPaymentInputException extends MoneyTrackerException {
-    private static final long serialVersionUID = 9099604506438509943L;
-
-    public InvalidPaymentInputException(String field) {
-        super("Cannot add payment. " + field + " is invalid.");
-    }
-}
